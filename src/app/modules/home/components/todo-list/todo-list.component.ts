03f675eadd937fd05937caf430e4f7683bb8eff1
Muriel Zounar <muriel.zounar@gmail.com>
2022-12-12 21:17:20 -0300

    App deploy using docs path

---
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -0,0 +1,57 @@
+import { Component, DoCheck, OnInit } from '@angular/core';
+
+//*INTERFACE
+import { TaskList } from './../../model/task-list';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss']
+})
+export class TodoListComponent implements OnInit, DoCheck{
+
+  public taskList: Array<TaskList> = JSON.parse(localStorage.getItem('list') || '[]')
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
+
+  ngDoCheck(): void {
+    this.setLocalStorage()
+  }
+
+  public deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1)
+  }
+
+  public deleteAllTasks() {
+    const confirm = window.confirm('Deseja realmente apagar todas as tarefas?')
+
+    if (confirm){
+     this.taskList = []
+    }    
+  }
+
+  public setEmitTaskList(event: string) {
+    this.taskList.push({task: event, checked: false})
+  }
+
+  public inputValidation(event: string, index: number) {
+    if (!event.length) {
+      const confirm = window.confirm('Tarefa em branco, deseja deletar?')
+
+      if (confirm) {
+        this.deleteItemTaskList(index)
+      }
+    }
+  }
+
+  public setLocalStorage() {
+    if (this.taskList) {
+      this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked))
+      localStorage.setItem('list', JSON.stringify(this.taskList)) 
+    }
+  }
+}
---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+//* COMPONENTS
+import { HeaderComponent } from './components/header/header.component';
+import { TodoButtonDeleteAllComponent } from './components/todo-button-delete-all/todo-button-delete-all.component';
+import { TodoInputAddItemsComponent } from './components/todo-input-add-items/todo-input-add-items.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+
+//* PAGES
+import { HomeComponent } from './pages/home/home.component';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    TodoButtonDeleteAllComponent,
+    TodoInputAddItemsComponent,
+    TodoListComponent,
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule
+  ]
+})
+export class HomeModule { }
